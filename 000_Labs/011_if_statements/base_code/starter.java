a3c3dee442cb366046f7e8b3946165ba8b5b308b
Rennix33 <rennixeoemusic@gmail.com>
2026-09-22 19:05:22 +0000

    update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -5,10 +5,33 @@
 
 import java.util.Scanner;
 
+
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter a integer: ");
+		int num1 = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("Please enter another integer: ");
+		int num2 = sc.nextInt();
+		sc.nextLine();
+
+		if( num1 != num2){
+			System.out.print("They are different.");
+
+		}
+
+		else if( num1 == num2){
+
+			System.out.print("They are the same");
+		}
+
+
+		
+
 	}
 }
